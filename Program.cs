a6a022f0a480060ca30a15116d78306d73cde0cc
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-11-08 17:15:06 +0530

    assign4

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine("The number is positive.");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("The number is negative.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is zero.");
+                }
+
+                string evenOrOdd = (number % 2 == 0) ? "even" : "odd";
+                Console.WriteLine("The number is " + evenOrOdd + ".");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
